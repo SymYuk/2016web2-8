a764b76341a6c47214acc6159f49b3711600768c
SymYuk <g1527043@st.t-kougei.ac.jp>
2016-11-21 15:55:11 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+onload=function()
+{
+	var canvas=document.getElementById('canvas');
+	canvas.width=500;
+	canvas.height=300;
+	
+	var gl=canvas.getContext(webgl')||canvas.getContent('experimental-webgl');
+	
+	gl.clearColor(0.0,0.0,0.0,1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+};
