7886d9c569617000e81183d5f28a96faa5ce7201
SymYuk <g1527043@st.t-kougei.ac.jp>
2016-11-21 16:26:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -35,6 +35,67 @@ onload=function()
 	
 	set_attribute([pos_vbo,col_vbo],attLocation,attStride);
 	
+	// シェーダ
+	  function create_shader(id){
+    // シェーダを格納する変数
+    var shader;
+
+    // HTMLからscriptタグへの参照を取得
+    var scriptElement = document.getElementById(id);
+
+    // scriptタグが存在しない場合は抜ける
+    if(!scriptElement){return;}
+
+    // scriptタグのtype属性をチェック
+    switch(scriptElement.type){
+    case 'x-shader/x-vertex':    // 頂点シェーダ
+      shader = gl.createShader(gl.VERTEX_SHADER);
+      break;
+    case 'x-shader/x-fragment': // ピクセルシェーダ
+      shader = gl.createShader(gl.FRAGMENT_SHADER);
+      break;
+    default :// それ以外
+      return;
+    }
+
+    // 生成されたシェーダにソースを割り当てる
+    gl.shaderSource(shader, scriptElement.text);
+
+    // シェーダをコンパイルする
+    gl.compileShader(shader);
+
+    // シェーダが正しくコンパイルされたかチェック
+    if(gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
+      return shader;    // 成功していたらシェーダを返して終了
+    }else{
+      // 失敗していたらエラーログをアラートする
+      alert(gl.getShaderInfoLog(shader));
+    }
+  }
+
+  // プログラムオブジェクトを生成しシェーダをリンクする関数
+  function create_program(vs, fs){
+    // プログラムオブジェクトの生成
+    var program = gl.createProgram();
+
+    // プログラムオブジェクトにシェーダを割り当てる
+    gl.attachShader(program, vs);
+    gl.attachShader(program, fs);
+
+    // シェーダをリンク
+    gl.linkProgram(program);
+
+    // シェーダのリンクが正しく行なわれたかチェック
+    if(gl.getProgramParameter(program, gl.LINK_STATUS)){
+      // 成功していたらプログラムオブジェクトを有効にする
+      gl.useProgram(program);
+      return program;      // プログラムオブジェクトを返して終了
+    }else{
+      // 失敗していたらエラーログをアラートする
+      alert(gl.getProgramInfoLog(program));
+    }
+}
+	
 	//VBO作成
 	function create_vbo(data)
 	{
